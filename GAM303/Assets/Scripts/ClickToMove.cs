871f631628e0e63efaa1a67c61cac59b9f95f4b4
Fabzman <33334777+Fabzman@users.noreply.github.com>
2018-10-03 16:10:45 +1000

    Exported Package
    Exported scripts and collectable scene from Rapid Prototyping

---
+++ GAM303/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ClickToMove : MonoBehaviour {
+
+
+
+    //public float smoothing = 7f;
+    //public Vector3 Target
+
+    //{
+    //    get { return target; }
+    //    set
+    //    {
+    //        target = value;
+
+    //        StopCoroutine("Movement");
+    //        StartCoroutine("Movement", target);
+    //    }
+    //}
+
+
+    //private Vector3 target;
+
+
+    //IEnumerator Movement(Vector3 target)
+    //{
+    //    while (Vector3.Distance(transform.position, target) > 0.05f)
+    //    {
+    //        transform.position = Vector3.Lerp(transform.position, target, smoothing * Time.deltaTime);
+
+    //        yield return null;
+    //    }
+    //}
+
+    //public PropertiesAndCoroutines coroutineScript;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider.gameObject != null)
+            {
+                Vector3 newTarget = hit.point + new Vector3(0, 0.5f, 0);
+                transform.DOMove(newTarget, 2f);
+                //coroutineScript.
+                //Target = newTarget;
+            }
+        }
+    }
+
+    //   // Use this for initialization
+    //   void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+}
---
+++ GAM303/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvents {
+
+    //Declare the events
+    public static event Action <int> OnScoreChange = null;
+
+    // Event reporting
+    public static void ReportOnScoreChange (int newValue)
+    {
+        if (OnScoreChange != null)
+            OnScoreChange(newValue);
+    }
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
